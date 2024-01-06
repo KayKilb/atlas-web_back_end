1860bcc099a289e73a90cd660c4ba711089fc2b3
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-01-05 20:15:39 -0600

    corcoutines

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""The list of the delays should be in ascending order without using sort() because of concurrency"""
+from typing import List
+import asyncio
+wait_random = __impor('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Async routine to spawn wait_random n times with specified max_delay"""
+    delay_list = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    return [await delay for delay in asyncio.as_completed(delay_list)]
