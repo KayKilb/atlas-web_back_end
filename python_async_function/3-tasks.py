f6f88a551764b4e3ae3e9f627fa04e0160b5c9ce
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-01-05 22:21:19 -0600

    checks failed due to asynchronous function call

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -4,8 +4,10 @@ Write a function task_wait_random that takes an
 integer max_delay and returns a asyncio.Task
 """
 import asyncio
-wait_random = __import__('0-basic_async_syntax.py').wait_random
+from typing import Any
 
-def task_wait_random(max_delay: int) -> asyncio.Task:
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+def task_wait_random(max_delay: int) -> Any:
     """Creates an asyncio Task for wait_random with specified max_delay"""
     return asyncio.create_task(wait_random(max_delay))
