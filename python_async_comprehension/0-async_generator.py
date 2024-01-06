6044c1bb452b896f5af2c2c9127a4865eab2ddf8
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-01-05 22:49:42 -0600

    generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,3 +8,9 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
+    """
+    the coroutine will loop 10 times
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
