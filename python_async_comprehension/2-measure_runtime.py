9d7e824aeb7963f33838f7053b6d459403005203
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-01-05 23:24:38 -0600

    runtime trial

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+measure_runtime coroutine that will execute async_comprehension
+four times in parallel using asyncio.gather
+"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+async def measure_runtime() -> float:
+    """
+    measure_runtime coroutine that will execute async_comprehension
+    """
+    start_time = time.perf_counter()
+    await asyncio.gather(*(async_comprehension.async_comprehension() for i in range(4)))
+    end_time = time.perf_counter()
+    total_time = end_time - start_time
+
+    return total_time
