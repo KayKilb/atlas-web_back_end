9553234fd09f0e0dabdb9f76ba1d98bb5140b5a3
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-01-05 23:28:45 -0600

    fixed call to .async_comprehension

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -7,12 +7,13 @@ import asyncio
 import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
+
 async def measure_runtime() -> float:
     """
     measure_runtime coroutine that will execute async_comprehension
     """
     start_time = time.perf_counter()
-    await asyncio.gather(*(async_comprehension.async_comprehension() for i in range(4)))
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
     end_time = time.perf_counter()
     total_time = end_time - start_time
 
