2c0a19a2ebddef682a740d8edf63a3be8959f01c
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-01-10 09:04:28 -0600

    basic caching

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Create a class BasicCache that inherits from
+BaseCaching and is a caching system
+"""
+from base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    """Child-class implementing key/value algorithm"""
+
+    def put(self, key, value):
+        """Inserting value into cache"""
+        if not key or value:
+            return
+        
+        self.cache_data[key] = value
+
+    def get(self, key):
+        """Retrieves value from cache"""
+        if not key:
+            return None
+        
+        return self.cache_data.get(key)
