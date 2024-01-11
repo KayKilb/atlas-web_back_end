ffe5e8acd37177df948df971e0e8e4743ce306be
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-01-11 09:59:40 -0600

    Reworking code to fix bug errors

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
-"""
-a method named get_page that takes two integer arguments page with
-default value 1 and page_size with default value 10
-"""
+"""Server data handler"""
 import csv
 import math
 from typing import List, Tuple
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
+    """
+    Server class to paginate a database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Constructor"""
         self.__dataset = None
 
     def dataset(self) -> List[List]:
@@ -28,23 +27,18 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Retrieve a specific page from the dataset."""
-        assert isinstance(page, int) and page > 0, "Page must be a positive integer."
-        assert isinstance(page_size, int) and page_size > 0, "Page size must be a positive integer."
-
-        start, end = index_range(page, page_size)
-        dataset = self.dataset()
-        if start >= len(dataset):
-            return []
-
-        return dataset[start:end]
+            """Return a poriton of the dataset given a page and page_size"""
+            assert(type(page_size) == int and type(page) == int)
+            assert(page > 0 and page_size > 0)
+            start_index, end_index = self.index_range(page, page_size)
+            return self.dataset()[start_index:end_index]
 
     def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
         """
-        return a tuple of size two containing a
-        start index and an end index corresponding
-        to the range of indexes to return in a list
-        for those particular pagination parameters
+            Return a tuple of size two containing a start index
+            and an end index corresponding to the range of indexes
+            to return in a list for those particular pagination
+            parameters
         """
         start_index = page_size * (page - 1)
         end_index = page_size * page
