81b5061ff48c6f3c14a1ce6e4c2257e748caffe4
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-01-10 22:42:38 -0600

    simple pagination

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+a method named get_page that takes two integer arguments page with
+default value 1 and page_size with default value 10
+"""
+import csv
+import math
+from typing import List, Tuple
+
+
+class Server:
+    """Server class that paginates database of baby names"""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        """Initialize"""
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+        
+    def get_page(self, page: int, page_size: int = 10)
+        """Returns portion of dataset given of page and page_size"""
+        assert(type(page_size) == int and type (page) == int)
+        assert(page > 0 and page_size > 0)
+        start_index, end_index = self.index_range(page, page_size)
+        return self.dataset()[start_index:end_index]
+
+    def index_range(page: int, page_size: int) -> Tuple[int, int]:
+        """
+        return a tuple of size two containing a
+        start index and an end index corresponding
+        to the range of indexes to return in a list
+        for those particular pagination parameters
+        """
+        start_index = page_size * (page - 1)
+        end_index = page_size * page
+
+    return (start_index, end_index)
