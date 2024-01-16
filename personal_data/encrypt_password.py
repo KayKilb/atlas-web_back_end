4b4dd018377e5bf91aed8a66ea06bc48e0448f35
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-01-16 13:34:49 -0600

    shortened statement for pycode

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -10,5 +10,5 @@ def hash_password(password: str) -> bytes:
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """function checks if a plaintext password matches a previously hashed password"""
+    """checks if a plaintext pw matches a prev hashed password"""
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
